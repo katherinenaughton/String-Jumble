dbcf6d8e0f5433e4016b50996ee3911006bfe03c
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-09-26 14:36:01 -0400

    Updated from Brython Server: 9/26/2018 2:36:01 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: Katie Naughton
-Credit: I worked alone. 
+Credit: I worked with Noah. 
 
 Assignment:
 
@@ -27,7 +27,7 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 
 text=input("Please enter a string of text (the bigger the better) : ")
-print("You entered " +text+ ". Now jumble it: ")
+print('You entered "' +text+ '". Now jumble it: ')
 
 #letters in reverse
 print(text[::-1])
