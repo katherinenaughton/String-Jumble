5aa063ae9e7de0a0d1b5bfc3d5c7cf8315c694b0
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-09-26 14:06:20 -0400

    Updated from Brython Server: 9/26/2018 2:06:20 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,8 +25,24 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
 text=input("Please enter a string of text (the bigger the better) : ")
 print("You entered " +text+ ". Now jumble it: ")
-
 print(text[::-1])
+
+
+wordlist=[]
+word=""
+for i in text[::-1]:
+    if i!=" ":
+        word=i+word
+    if i==" ":
+        wordlist.append(word)
+        word=""
+wordlist.append(word)
+print(wordlist[:])
+
+for i in text[::-1]:
+    if 
+
 
